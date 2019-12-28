bd042b4bc20aa62ace8c89baa39c35b25a3ab364
bashard001 <bdaseh@gmail.com>
2019-12-28 13:28:39 -0800

    done the posting items to the database functionality

--- greatBayBasic.js
+++ greatBayBasic.js
@@ -8,7 +8,7 @@ var connection = mysql.createConnection({
     user: "root",
 
     password: "wakeland008",
-    database: "greatBay_db"
+    database: "greatBay_DB"
 
 });
 
@@ -28,5 +28,55 @@ function start(){
       message: "Would you like to [POST] an auction or [BID] on an auction?",
       choices: ["POST", "BID", "EXIT"]
 
+    }).then(function(answer){
+        if (answer.postOrBid === "POST"){
+
+            postItem();
+
+        } else if(answer.postOrBid === "BID"){
+
+            bidItem();
+        
+
+        }else(
+            connection.end()
+        )
     })
+}
+
+function postItem(){
+    inquirer.prompt([{
+        name:"item",
+        type:"input",
+        message: "what is the item you would like to submit?"
+    },
+{
+    name: "category",
+    type: "input",
+    message: "What category would you like to place your auction in?"
+},
+{
+    name: "startingBid",
+        type: "input",
+        message: "What would you like your starting bid to be?",
+        validate: function(value) {
+          if (isNaN(value) === false) {
+            return true;
+          }
+          return false;
+        }
+}]).then(function(answer){
+    connection.query("insert into auctions set ?",{
+        item_name: answer.item,
+          category: answer.category,
+          starting_bid: answer.startingBid || 0,
+          highest_bid: 1000000
+    },
+    function(err) {
+        if (err) throw err;
+        console.log("Your auction was created successfully!");
+        // re-prompt the user for if they want to bid or post
+        start();
+      })
+})
 }
